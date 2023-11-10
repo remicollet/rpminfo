18ecce5dd8b1f39fbcdc8da17bac13a05caca6b7
Remi Collet <remi@remirepo.net>
2023-11-10 10:54:13 +0100

    back to dev

--- php_rpminfo.h
+++ php_rpminfo.h
@@ -22,7 +22,7 @@
 extern zend_module_entry rpminfo_module_entry;
 #define phpext_rpminfo_ptr &rpminfo_module_entry
 
-#define PHP_RPMINFO_VERSION "1.1.0"
+#define PHP_RPMINFO_VERSION "1.1.1-dev"
 
 #ifdef ZTS
 #include "TSRM.h"
